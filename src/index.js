234d09efcc2fd5337f1cc56691681d5560d8c71a
pineladsn <bruno.pinelamarques@gmail.com>
2020-07-29 18:03:40 -0300

    Adjustments after a wrong merge

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Import main html file
+import html from "./index.pug"
+
+// Import styles
+import style from "./styles/main.sass"
+
+// Import scripts
+import script from "./scripts/plugin.js"
+
+console.log('all files loaded')
